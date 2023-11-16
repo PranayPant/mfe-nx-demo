6afdd556212bb69b43832def90d1e6b511c91caa
PranayPant <ppantg1@gmail.com>
2023-11-15 21:23:16 -0600

    feat: add storybook

---
+++ about/src/app/about-app.stories.tsx
@@ -0,0 +1,10 @@
+import App from './app';
+
+export default {
+  title: 'Apps/About',
+  component: App,
+};
+
+export const LandingPage = {
+  render: () => <App />,
+};
---
+++ main/src/app/main-app.stories.tsx
@@ -0,0 +1,10 @@
+import App from './app';
+
+export default {
+  title: 'Apps/Main',
+  component: App,
+};
+
+export const LandingPage = {
+  render: () => <App />,
+};
